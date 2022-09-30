00547cd1dc8ec242128d506463bd27f38d1f0778
Ivan Filippov <iffilippov@yandex.ru>
2022-09-30 16:25:02 +0300

    Modified homework.py: base and inherited classes are written

--- homework.py
+++ homework.py
@@ -11,15 +11,19 @@ class Training:
                  duration: float,
                  weight: float,
                  ) -> None:
-        pass
+        self.action = action
+        self.duration = duration
+        self.weight = weight
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        pass
+        M_IN_KM = 1000
+        LEN_STEP = 0.65
+        self.distance = self.action * LEN_STEP / M_IN_KM
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        pass
+        self.mean_speed = self.distance / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -32,17 +36,75 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-    pass
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 ) -> None:
+        super().__init__(action, duration, weight)
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий при беге."""
+        coeff_calorie_1 = 18
+        coeff_calorie_2 = 20
+        M_IN_KM = 1000
+        self.spent_calories = ((coeff_calorie_1 * self.mean_speed
+                               - coeff_calorie_2) * self.weight
+                               / M_IN_KM * self.duration)
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    pass
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 height: float
+                 ) -> None:
+        super().__init__(action, duration, weight)
+        self.height = height
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий при беге."""
+        coeff_calorie_1 = 0.035
+        coeff_calorie_2 = 0.029
+        self.spent_calories = (coeff_calorie_1 * self.weight
+                               + (self.mean_speed ** 2 // self.height)
+                               * coeff_calorie_2 * self.weight) * self.duration
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    pass
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 length_pool: float,
+                 count_pool: int
+                 ) -> None:
+        super().__init__(action, duration, weight)
+        self.length_pool = length_pool
+        self.count_pool = count_pool
+
+    def get_distance(self) -> float:
+        """Получить дистанцию в км."""
+        M_IN_KM = 1000
+        LEN_STEP = 1.38
+        self.distance = self.action * LEN_STEP / M_IN_KM
+
+    def get_mean_speed(self) -> float:
+        """Получить среднюю скорость движения."""
+        M_IN_KM = 1000
+        self.mean_speed = (self.length_pool * self.count_pool
+                           / M_IN_KM / self.duration)
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий при беге."""
+        coeff_calorie_1 = 1.1
+        coeff_calorie_2 = 2
+        self.spent_calories = ((self.mean_speed + coeff_calorie_1)
+                               * coeff_calorie_2 * self.weight)
 
 
 def read_package(workout_type: str, data: list) -> Training:
@@ -65,4 +127,3 @@ if __name__ == '__main__':
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
-
